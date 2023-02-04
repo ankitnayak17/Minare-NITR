7a6dd40c1d40cc900b4420d39a72cc143adc8801
Ankit Kumar Nayak <ankitnayak1726@gmail.com>
2023-02-04 21:38:32 +0530

    created Page teams

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, Router } from "react-router-dom";
+
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import Team from './pages/teams';
+import Body from './pages/body';
+
+// import { Hashroute }
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Banner />
+      {/* <Banner />
       <Skills />
       <Projects />
-      <Contact />
-      <Footer />
+      <Contact /> */}
+      <Body />
+      
+        {/* <Routes>
+          <Route path="/" element={<Body/>} />
+          <Route path="/team" element={<Team/>} />
+        </Routes> */}
+      <Team />
+      {/* <Footer /> */}
     </div>
   );
 }
