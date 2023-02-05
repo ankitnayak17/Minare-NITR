e272ca855b21b413e354fb850e033ac06b53a056
Ankit Kumar Nayak <ankitnayak1726@gmail.com>
2023-02-06 01:23:07 +0530

    navbar fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,13 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
+// import Nitlogo from '../assets/img/nit logo.svg';
+// import logo from '../assets/img/MES logo.svg';
+// import logo2 from '../assets/img/Minare logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
-
-import { Link } from "react-router-dom";
+import { Link, NavLink} from "react-router-dom";
 
 export const NavBar = () => {
 
@@ -35,37 +33,39 @@ export const NavBar = () => {
   }
 
   return (
-    // <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
+            {/* <img src={logo} alt="Logo" />
+            <img src={logo2}  alt="Logo" /> */}
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              {/* <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
               <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link> */}
               {/* <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link> */}
-              <Link to={'/'}>Home</Link>
-              <Link to={'/team'}>Teams</Link>
+              <NavLink to={'/'} className={'navbar-link nav-link'} activeClassName="active">Home</NavLink>
+              <NavLink to={'/team'} className={'navbar-link nav-link'} activeClassName="active">Teams</NavLink>
+              {/* <NavLink to={'/#skills'} className={'navbar-link nav-link'} activeClassName="active">skills</NavLink> */}
+              <HashLink to={'/#events'} onClick={() => onUpdateActiveLink('Event')} className={'navbar-link nav-link'}>Events</HashLink>
+              <NavLink to={'/Sponserships'} className={'nav-link navbar-link'} activeClassName="active">Sponsers</NavLink>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/company/mining-engineering-society-nit-rourkela/" rel="noreferrer" target={"_blank"}><img src={navIcon1} alt="" /></a>
+                <a href="https://www.facebook.com/minare.nitr/" rel="noreferrer" target={"_blank"}><img src={navIcon2} alt="" /></a>
+                <a href="https://instagram.com/minare.nitr?igshid=YmMyMTA2M2Y=" rel="noreferrer" target={"_blank"}><img src={navIcon3} alt="" /></a>
               </div>
-              {/* <HashLink to='#connect'>
+              <HashLink to='/#connect'>
                 <button className="vvd"><span>Let's Connect</span></button>
-              </HashLink> */}
+              </HashLink>
             </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    // </Router>
   )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -42,7 +42,7 @@ export const Projects = () => {
   ];
 
   return (
-    <section className="project" id="project">
+    <section className="project" id="events">
       <Container>
         <Row>
           <Col size={12}>
