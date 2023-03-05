0562e583fa68464cf0bf4fefaaa5e2a562e5a15c
Ankit Kumar Nayak <ankitnayak1726@gmail.com>
2023-03-05 18:33:54 +0530

    spon update

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -4,6 +4,7 @@ import image2 from "../assets/img/spon/mcllogo.png";
 import image3 from "../assets/img/spon/moil.png";
 import image4 from "../assets/img/spon/omclogo.png";
 import image5 from "../assets/img/spon/rc_rungta_group.png";
+import image6 from "../assets/img/spon/handshake.jpg";
 
 function Section1() {
   var companies = [
@@ -34,9 +35,9 @@ function Section1() {
         <div className="spon-card" key={i}>
           <div className={`span-card${i}`}>
             <a href={element.Link}>
-                {/* <img src="aditya.jpg" alt=""> */}
-                    <img className='team-card-face-img' src={element.Image} alt={`Img-${element.Name}`}></img>
-                </a>
+              {/* <img src="aditya.jpg" alt=""> */}
+              <img className='team-card-face-img' src={element.Image} alt={`Img-${element.Name}`}></img>
+            </a>
           </div>
           <div className="card-body">
             {element['Name']}
@@ -47,29 +48,86 @@ function Section1() {
     return <>{l}</>;
   }
 
-  return(
+  return (
     <>
-        <div class="spon-container">
-            <div class="spon-heading">
-            <h1>FEATURED SPONSORS</h1>
-            <p>
-                Over the past years Minare has had the privilege to have hosted a number
-                of sponsors which provided a very entertaining experience to our
-                visitors as well as the brand.
-            </p>
-            </div>
-            <div className="spon-row">
-                <Companies></Companies>
-            </div>
+      <div className="spon-container">
+        <div className="spon-heading">
+          <h1>FEATURED SPONSORS</h1>
+          <p>
+            Over the past years Minare has had the privilege to have hosted a number
+            of sponsors which provided a very entertaining experience to our
+            visitors as well as the brand.
+          </p>
         </div>
+        <div className="spon-row">
+          <Companies></Companies>
+        </div>
+      </div>
     </>
   )
 }
 
 function Sponsors() {
   return (
-    <div className="spon-row">
-      <Section1></Section1>
+    // <div className="spon-row">
+    //   <Section1></Section1>
+    // </div>
+    <div className="spon-n-container-main">
+
+      <div className="spon-n-container">
+        <div className="spon-n-heading">
+          <h1 className="spon-n-title">FEATURED SPONSORS</h1>
+          <div className="spon-n-content">
+            <div className="spon-n-head">Over the past years Minare has had the privilege to have hosted a number of sponsors which provided a very entertaining experience to our visitors as well as the brand. The crowd engagement, media exposure, sampling and brand building opportunities offered at Minare are unparalleled.</div>
+            <div className="spon-n-imd">
+
+              <img src={image6} alt="" />
+            </div>
+          </div>
+        </div>
+        <h1 className="spon-n-head1">OUR SPONSORS</h1>
+        <div className="spon-n-slider">
+          <div className="spon-n-slide-track">
+            <div className="spon-n-slide">
+              <img src={image0} alt="" />
+            </div>
+            <div className="spon-n-slide">
+              <img src={image1} alt="" />
+            </div>
+            <div className="spon-n-slide">
+              <img src={image2} alt="" />
+            </div>
+            <div className="spon-n-slide">
+              <img src={image3} alt="" />
+            </div>
+            <div className="spon-n-slide">
+              <img src={image4} alt="" />
+            </div>
+            <div className="spon-n-slide">
+              <img src={image5} alt="" />
+            </div>
+
+            <div className="spon-n-slide">
+              <img src={image0} alt="" />
+            </div>
+            <div className="spon-n-slide">
+              <img src={image1} alt="" />
+            </div>
+            <div className="spon-n-slide">
+              <img src={image2} alt="" />
+            </div>
+            <div className="spon-n-slide">
+              <img src={image3} alt="" />
+            </div>
+            <div className="spon-n-slide">
+              <img src={image4} alt="" />
+            </div>
+            <div className="spon-n-slide">
+              <img src={image5} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
