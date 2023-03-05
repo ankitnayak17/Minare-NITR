e01a74d26f24f1ffbe9801427e5efff1fd7d3be2
Ankit Kumar Nayak <ankitnayak1726@gmail.com>
2023-03-05 23:31:46 +0530

    fixed project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,14 +1,14 @@
-import { Col } from "react-bootstrap";
+import { Col, Button } from "react-bootstrap";
 
 export const ProjectCard = ({ title, description, button, imgUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} />
+        <img src={imgUrl} alt='img' />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
-          <Button>{buuton}</Button>
+          <Button>{button}</Button>
         </div>
       </div>
     </Col>
